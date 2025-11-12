64715cc292ed74aaae5b5143c55bc59e4174fdb1
LF236 <fernandorodriguez236@gmail.com>
2025-11-12 17:02:29 -0600

    Adding cors and modifiying the docker-comose

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { ValidationError } from 'class-validator';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.setGlobalPrefix('api');
+	app.enableCors({
+		origin: '*',
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+		credentials: true
+	})
 	app.useGlobalPipes(
 		new ValidationPipe({
 			//whitelist: true,
